6b4cb82954dacf12dc15f96895854c387ed81e95
Karan Oza <50609276+karanoza@users.noreply.github.com>
2025-03-06 13:57:32 +0530

    dynamic props values from data file

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,12 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.title}></img>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title}></img>
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
@@ -40,15 +40,16 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title="Components"
-              description="The core UI building block"
-              image={ComponentsImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept  title="Props"
-              description="The core UI building block"
-              image={ComponentsImg}/>
-            <CoreConcept title="Components"/>
-            <CoreConcept title="Components"/>
+
+            {/* below by usng spread operator syntax  */}
+
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
